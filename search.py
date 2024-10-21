e26eb8c8ddb3cc235cb9c4d81304b0f814ae8090
Shy <174926575+shy913@users.noreply.github.com>
2024-10-21 20:06:22 +0800

    Fix bugs, Added bisection search

--- search.py
+++ search.py
@@ -3,6 +3,8 @@ epsilon = 1e-5
 cache = [-1] * 100
 cache[0] = 0
 cache[1] = 1
+
+
 def fib(n):
     global cache
     if n <= 1:
@@ -28,31 +30,51 @@ def fib_search(f, a, b, n):
     elif n == 2:
         mid = (a + b) / 2
         if f(mid - epsilon) < f(mid + epsilon):
+            print(f"len: {(b - a)/2}")
             print(f"[{a}, {mid}]")
             return a, mid
         elif f(mid - epsilon) > f(mid + epsilon):
+            print(f"len: {(b - a)/2}")
             print(f"[{mid}, {b}]")
             return mid, b
         else:
             print(f"[{mid - epsilon}, {mid + epsilon}]")
             return mid - epsilon, mid + epsilon
     else:
+        print(f"len: {b-a}")
         left_eval = (a + (b - a) * fib(n - 1) / fib(n + 1))
         right_eval = (a + (b - a) * fib(n) / fib(n + 1))
-        print(f"-----------------\n[{a},{left_eval},{right_eval},{b}]")
+        print(f"[{a},{left_eval},{right_eval},{b}]")
         if f(left_eval) < f(right_eval):
-            print(f"left_eval < right_eval!\n-----------------\n")
+            # print(f"left_eval < right_eval!\n-----------------\n")
             fib_search(f, a, right_eval, n - 1)
         elif f(left_eval) == f(right_eval):
-            print(f"right_eval = left_eval!\n-----------------\n")
-            fib_search(f, left_eval,right_eval, n - 1)
+            # print(f"right_eval = left_eval!\n-----------------\n")
+            fib_search(f, left_eval, right_eval, n - 1)
         else:
-            print(f"right_eval > left_eval!\n-----------------\n")
+            # print(f"right_eval > left_eval!\n-----------------\n")
             fib_search(f, left_eval, b, n - 1)
 
 
-def func(x):
-    return x ** 2 - 6 * x + 2
+def binary_search(f, a, b, n):
+    global epsilon
+    mid = (a + b) / 2
+    if n == 0 or n == 1:
+        print(f"len: {b-a}")
+        print(f"[{a}, {b}]")
+        return a, b
+    elif f(mid - epsilon) < f(mid + epsilon):
+        print(f"[{a}, {mid}]")
+        return binary_search(f, a, mid, n-2)
+    elif f(mid - epsilon) > f(mid + epsilon):
+        print(f"[{mid}, {b}]")
+        return binary_search(f, mid, b, n-2)
 
 
-fib_search(func, 0, 10, 34)
+if __name__ == "__main__":
+    def func(x):
+        return x ** 2 - 6 * x + 2
+    print("fibonacci search:")
+    fib_search(func, 0, 10, 8)
+    print("\nbinary search:")
+    binary_search(func, 0, 10, 12)
